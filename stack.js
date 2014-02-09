8fc862b0ce603eeb5441caf7aa27e794ce3c73dc
unknown <sy6765856@gmail.com>
2014-02-10 00:04:44 +0800

    sunday

--- stack.js
+++ stack.js
@@ -1,19 +1,19 @@
 /**
  * Created with JetBrains WebStorm.
  * User: sy6765856
- * Date: 14-1-20
- * Time: 下午9:39
+ * Date: 14-2-10
+ * Time: 上午12:02
  * To change this template use File | Settings | File Templates.
  */
-Heap = function(){
-    return extend(Tree, {
-        run: function(id) {
+Stack = function() {
+    return extend(AlgorithmBase, {
+        init: function() {
             return this;
         },
-        insert: function() {
+        push: function() {
             return this;
         },
-        maintain: function() {
+        pop: function() {
             return this;
         }
     });
