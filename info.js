027e1c47d33fd5f6b06da4a9e10a7f01199ade5a
unknown <sy6765856@gmail.com>
2014-05-02 23:03:59 +0800

    fj

---
+++ info.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: sy6765856
+ * Date: 14-5-2
+ * Time: 下午5:25
+ * To change this template use File | Settings | File Templates.
+ */
+Info = function() {
+    return {
+
+    };
+}();
